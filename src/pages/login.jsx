cd49845bdcf7d49cf06078f82dab678e58e76c9f
meparthka <parthka.2005@gmail.com>
2022-07-05 19:39:28 +0530

    clear login form

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,27 +3,10 @@ import Navbar from "../components/navbar"
 import InputBox from "../components/inputbox"
 import CheckBox from "../components/checkbox"
 import { BiUser, BiLockAlt } from "react-icons/bi";
-import { useDispatch } from 'react-redux'
-import { addNotify } from "../data/notify"
 
 const Login = () => {
 
-  const dispatch = useDispatch();
-
-  const addNoti = () => {
-    dispatch(addNotify({ timeOut: 3000 }))
-  }
-
-  const addNoti2 = () => {
-    dispatch(addNotify({}))
-  }
-
   let [loginValue, setLoginValues] = useState({})
-
-  const viewValue = (e) => {
-    e.preventDefault()
-    console.log(loginValue);
-  }
 
   return (
     <div>
@@ -41,13 +24,10 @@ const Login = () => {
             </div>
             <div className="inputs">
               <InputBox icon={<BiUser />} name="username" values={loginValue} setValues={setLoginValues} place="Username" />
-              <InputBox icon={<BiLockAlt />} name="password" values={loginValue} setValues={setLoginValues}  place="Password" type="password" />
-              <CheckBox lable="Remember Me." name="me" values={loginValue} setValues={setLoginValues}  />
-            </div>
-            <button onClick={viewValue} className="btn btn-primary a-right">Login</button>
+              <InputBox icon={<BiLockAlt />} name="password" values={loginValue} setValues={setLoginValues} place="Password" type="password" />
+              <CheckBox lable="Remember Me." name="remember" values={loginValue} setValues={setLoginValues} />            </div>
+            <button className="btn btn-primary a-right">Login</button>
           </form>
-          <button onClick={addNoti}>TimeOut Notify</button>
-          <button onClick={addNoti2}>Notify</button>
         </div>
       </div>
     </div>
