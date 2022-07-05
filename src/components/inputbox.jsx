83cf664f8aaad8a78adca4f48095fa591e62e7c3
meparthka <parthka.2005@gmail.com>
2022-07-05 20:46:52 +0530

    add input error

--- src/components/inputbox.jsx
+++ src/components/inputbox.jsx
@@ -8,21 +8,24 @@ const InputBox = (q) => {
     setInputValue(e.target.value);
 
     if (q.setValues) {
-      q.setValues({...q.values || {}, [q.name]: e.target.value})
+      q.setValues({ ...q.values || {}, [q.name]: e.target.value })
     }
   }
 
   return (
-    <div className={"input-box" + (q.icon ? "-alt" : "") + " " + q.className}>
-      {(() => {
-        if (q.icon)
-          return (
-            <div className="text">
-              {q.icon}
-            </div>
-          )
-      })()}
-      <input type={q.type || "text"} name={q.name} onChange={setValue} className="m-0" placeholder={q.place} />
+    <div className={"input-box" + (q.icon ? "-alt" : "") + (q.className ? (" " + q.className) : "") + (q.error[q.name] ? " input-error" : "")}>
+      <div className="input-input-box">
+        {(() => {
+          if (q.icon)
+            return (
+              <div className="text">
+                {q.icon}
+              </div>
+            )
+        })()}
+        <input type={q.type || "text"} name={q.name} onChange={setValue} className="m-0" placeholder={q.place} />
+      </div>
+      <span className="input-error">{q.error[q.name]}</span>
     </div>
   )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,7 +6,34 @@ import { BiUser, BiLockAlt } from "react-icons/bi";
 
 const Login = () => {
 
-  let [loginValue, setLoginValues] = useState({})
+  let [loginValue, setLoginValues] = useState({
+    username: "",
+    password: "",
+    remember: false
+  })
+
+  let [loginError, setLoginError] = useState({})
+
+  const tryLogin = (e) => {
+    e.preventDefault()
+    let usernameError, passwordError
+
+    if (loginValue.username === "") {
+      usernameError = "Please Enter Username"
+    }
+
+    if (loginValue.password === "") {
+      passwordError = "Please Enter Password"
+    }
+
+    setLoginError({
+      username: usernameError,
+      password: passwordError
+    })
+
+    console.log(loginValue);
+
+  }
 
   return (
     <div>
@@ -16,16 +43,17 @@ const Login = () => {
           <div className="title">
             <span className="">Login</span>
           </div>
-          <form className="form">
+          <form onSubmit={tryLogin} className="form">
             <div className="overlay d-none">
               <div>
                 <div class="loader"></div>
               </div>
             </div>
             <div className="inputs">
-              <InputBox icon={<BiUser />} name="username" values={loginValue} setValues={setLoginValues} place="Username" />
-              <InputBox icon={<BiLockAlt />} name="password" values={loginValue} setValues={setLoginValues} place="Password" type="password" />
-              <CheckBox lable="Remember Me." name="remember" values={loginValue} setValues={setLoginValues} />            </div>
+              <InputBox icon={<BiUser />} name="username" values={loginValue} setValues={setLoginValues} error={loginError} place="Username" />
+              <InputBox icon={<BiLockAlt />} name="password" values={loginValue} setValues={setLoginValues} error={loginError} place="Password" type="password" />
+              <CheckBox lable="Remember Me." name="remember" values={loginValue} setValues={setLoginValues} />
+            </div>
             <button className="btn btn-primary a-right">Login</button>
           </form>
         </div>
