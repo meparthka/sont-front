e4ad2ee09021321596240a101196055ec6b60f31
meparthka <parthka.2005@gmail.com>
2022-07-07 21:43:26 +0530

    add X App

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,6 +6,7 @@ import NotFound from "./notfound"
 import store from '../data/store'
 import { Provider } from 'react-redux'
 import { NotificationProvider } from "../util/notification"
+import AppX from "./x/X"
 
 const adminApp = () => {
   return (
@@ -15,6 +16,7 @@ const adminApp = () => {
           <Routes>
             <Route path="*" element={<NotFound />} />
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/x" element={<AppX />} />
             <Route exact path="/login" element={<Login />} />
           </Routes>
         </div>
